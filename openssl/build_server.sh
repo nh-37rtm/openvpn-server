573dd2fd37281c12a638bc9d68f010aa3bfa6f3d
heim nicolas <heim.nico@gmail.com>
2024-05-23 12:19:52 +0200

    error

--- openssl/build_server.sh
+++ openssl/build_server.sh
@@ -5,20 +5,15 @@ set -euxab
 {
     cd $(dirname -- "$0")
     if ! [[ -f ca-key.pem ]]
-
-        # generating ca key ...
-        openssl genpkey -algorithm RSA -out ca-key.pem -config ./openssl.conf
-
-        # generating new ca certificate with key ...
-        openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 365 -config ./openssl.conf
-
     then
 
-    # creating a new certificate request for server with key ...
-    openssl req -new -nodes -keyout server.key -out server.csr -config ./openssl.conf
+        # creating a new certificate request for server with key ...
+        openssl req -new -nodes -keyout server.key -out server.csr -config ./openssl.conf
 
-    # signing certificate request ...
-    openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
+        # signing certificate request ...
+        openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
+
+    fi
 
     if ! [[ -f dh2048.pem ]]
     then
