5ccd88b8dda62a9ea91410250a5bdd22510a9c07
heim nicolas <heim.nico@gmail.com>
2024-05-23 22:18:42 +0200

    easyrsa

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM debian:bookworm-slim as base
 
-
 ARG DEBIAN_FRONTEND=noninteractive
 ENV VIRTUAL_ENV=/opt/python/venv \
     APP_USER=app \
@@ -11,18 +10,23 @@ USER root:root
 # python execution environment
 RUN apt-get update
 RUN apt-get install -y --no-install-recommends \
-    openvpn openssl jq iproute2
-
-# Create a dedicated user
-# RUN useradd -ms /bin/bash openvpn
+    openvpn
 
 RUN mkdir -p \
     /etc/openvpn/server/client-configs /var/openvpn/
 
 RUN ln -s /etc/openvpn/openssl /etc/openvpn/server/openssl
-
 WORKDIR /etc/openvpn/server
-# RUN openvpn --genkey secret ta.key
+
+COPY easyrsa/pki/private/ascs-npt.key /etc/openvpn/server/openssl/server-key.pem
+COPY easyrsa/pki/issued/ascs-npt.crt /etc/openvpn/server/openssl/server-cert.pem
+COPY easyrsa/pki/private/ca.key /etc/openvpn/server/openssl/ca-key.pem
+COPY easyrsa/pki/ca.crt /etc/openvpn/server/openssl/ca-cert.pem
+COPY easyrsa/pki/dh.pem /etc/openvpn/server/openssl/dh.pem
+
+# Create a dedicated user
+# RUN useradd -ms /bin/bash openvpn
+# RUN openvpn --genkey secret /etc/openvpn/server/openssl/ta.key
 
 ENTRYPOINT [ "/usr/sbin/openvpn" ]
 
--- openssl/build_server.sh
+++ openssl/build_server.sh
@@ -9,16 +9,20 @@ set -euxab
 
         # generating ca key ...
         openssl genpkey -algorithm RSA -out ca-key.pem -config ./openssl.conf
+        # openssl req -new -sha256 -nodes -newkey rsa:4096 -keyout ca-key.pem -out ca.csr
 
         # generating new ca certificate with key ...
         openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 365 -config ./openssl.conf
+        # openssl x509 -req -sha256 -extfile x509.ext -extensions ca -in ca.csr -signkey ca-key.pem -days 1095 -out ca-cert.pem
     fi
 
     # creating a new certificate request for server with key ...
     openssl req -new -nodes -keyout server.key -out server.csr -config ./openssl.conf
+    # openssl req -new -sha256 -nodes -newkey rsa:4096 -keyout server.key -out server.csr -config ./openssl.conf
 
     # signing certificate request ...
-    openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
+    # openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
+    openssl x509 -req -sha256 -CA ca-cert.pem -CAkey ca-key.pem -days 730 -CAcreateserial -extfile x509.ext -extensions server -in server.csr -out server.crt
 
     if ! [[ -f dh2048.pem ]]
     then
