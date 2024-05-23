855afcbebca1a894fd4c49a01a4ee37dec88aa9e
heim nicolas <heim.nico@gmail.com>
2024-05-23 14:18:35 +0200

    fix

--- openssl/build_server.sh
+++ openssl/build_server.sh
@@ -7,13 +7,18 @@ set -euxab
     if ! [[ -f ca-key.pem ]]
     then
 
-        # creating a new certificate request for server with key ...
-        openssl req -new -nodes -keyout server.key -out server.csr -config ./openssl.conf
+        # generating ca key ...
+        openssl genpkey -algorithm RSA -out ca-key.pem -config ./openssl.conf
 
-        # signing certificate request ...
-        openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
-
+        # generating new ca certificate with key ...
+        openssl req -new -x509 -key ca-key.pem -out ca-cert.pem -days 365 -config ./openssl.conf
     fi
+
+    # creating a new certificate request for server with key ...
+    openssl req -new -nodes -keyout server.key -out server.csr -config ./openssl.conf
+
+    # signing certificate request ...
+    openssl x509 -req -days 365 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server.crt
 
     if ! [[ -f dh2048.pem ]]
     then
