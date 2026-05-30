e466830b38538c6576df5a6a1031251f3ddb4e65
heim nicolas <heim.nico@gmail.com>
2026-05-30 16:33:57 +0200

    !

--- scripts/generate_configuration.sh
+++ scripts/generate_configuration.sh
@@ -1,7 +1,11 @@
 set -eux
 
-. $VIRTUAL_ENV/bin/activate && \
-    for proto in tcp udp; do \
-         SERVER_PROTO=$proto jte -t ./conf/templates/openvpn_server.conf.j2 \
-             > /opt/openvpn/conf/openvpn_${proto}.conf; \
-    done
+. $VIRTUAL_ENV/bin/activate
+
+for proto in tcp udp; 
+do
+    SERVER_PROTO=$proto jte -t ./conf/templates/openvpn_server.conf.j2 > \
+        /opt/openvpn/conf/openvpn_${proto}.conf
+done
+
+# exec openvpn $@
